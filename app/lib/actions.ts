c91bb47848cee9610c6dc9066d0da3a2640ef653
Yuko Higuchi <39221794+YukoHiguchi@users.noreply.github.com>
2023-11-27 12:34:59 -0500

    feat: add authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { sql } from '@vercel/postgres';
 import { z } from 'zod'
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+
 
 const FormSchema = z.object({
   id: z.string(),
@@ -109,4 +111,19 @@ export async function deleteInvoice(id: string) {
     }
   }
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+
+  } catch (error){
+    if((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
+  }
 }
